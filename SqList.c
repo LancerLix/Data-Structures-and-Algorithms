013ead4d27285b612e54a9c5c8ead3e9720840a9
Lix <lancer.k.lix@gmail.com>
2019-01-15 18:42:10 +0800

    Update SqList.c
    accomplish sqlist, add main() to test functions

--- SqList.c
+++ SqList.c
@@ -1,7 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 #define LIST_INIT_SIZE 100
 #define LIST_SIZE_INCREMENT 10
 
-typedef int ElemType
+typedef int ElemType;
 
 typedef struct {
 	ElemType* elem_base_addr;
@@ -9,109 +12,222 @@ typedef struct {
 	int max_length;
 }SqList;
 
-int InitSqList(SqList& L) {
-	L.elem_base_addr = (ElemType*)malloc(LIST_INIT_SIZE * sizeof(ElemType));
-	if(!L.elem_base_addr) {
+int InitSqList(SqList* L) {
+	if(!L) {
+		//illegal operation
+		return -1;
+	}
+		
+	L->elem_base_addr = (ElemType*)malloc(LIST_INIT_SIZE * sizeof(ElemType));
+	if(!L->elem_base_addr) {
 		return -1;
 	}
 	
-	L.cur_length = 0;
-	L.max_length = LIST_INIT_SIZE;
+	L->cur_length = 0;
+	L->max_length = LIST_INIT_SIZE;
 	
 	return 0;
 }
 
-int SqListInsert(SqList& L, int index, ElemType elem) {
-	if(index < 0 || index > L.length + 1) {	
+int SqListInsert(SqList* L, int index, ElemType elem) {
+	if(!L) {
+		//illegal operation
 		return -1;
 	}
 	
-	if(L.cur_length == L.max_length) {
-		ElemType* new_base_addr = (ElemType*) realloc(L.elem_base_addr,
-								(L.max_length + LIST_SIZE_INCREMENT) * sizeof(ElemType));
+	if(index < 1 || index > L->cur_length + 1) {	
+		//parameter is not legal
+		return -1;
+	}
+	
+	if(!L->elem_base_addr) {
+		//SqList is not init
+		return -1;
+	}
+	
+	if(L->cur_length == L->max_length) {
+		ElemType* new_base_addr = (ElemType*) realloc(L->elem_base_addr,
+								(L->max_length + LIST_SIZE_INCREMENT) * sizeof(ElemType));
 		
 		if(!new_base_addr) {
 			return -1;
 		}
 		
-		L.elem_base_addr = new_base_addr;
-		L.max_length += LIST_SIZE_INCREMENT;
+		L->elem_base_addr = new_base_addr;
+		L->max_length += LIST_SIZE_INCREMENT;
 	}
 	
-	ElemType* insert_location = &L.elem_base_addr[index - 1];
-	for(ElemType* p = &L.elem_base_addr[L.cur_length - 1]; p >= insert_location; p--) *(p + 1) = *p;
+	ElemType* insert_location = &L->elem_base_addr[index - 1];
+	for(ElemType* p = &L->elem_base_addr[L->cur_length - 1]; p >= insert_location; p--) *(p + 1) = *p;
 	
 	*(insert_location) = elem;
-	L.cur_length++;
+	L->cur_length++;
 	
 	return 0;
 }
 
-int SqListDelete(SqList& L, int index) {
-	if(index < 0 || index > L.length) {
+int SqListDelete(SqList* L, int index) {
+	if(!L) {
+		//illegal operation
 		return -1;
 	}
 	
-	ElemType* delete_location = L.elem_base_addr[index - 1];
-	for(ElemType* p = &L.elem_base_addr[L.cur_length -1]; p > delete_location; p--) *(p - 1) = *p;
+	if(index < 1 || index > L->cur_length) {
+		//parameter is not legal
+		return -1;
+	}
 	
-	L.cur_length--;
+	if(!L->elem_base_addr) {
+		//SqList is not init
+		return -1;
+	}
+	
+	ElemType* delete_location = &L->elem_base_addr[index - 1];
+	for(ElemType* p = delete_location; p <= &L->elem_base_addr[L->cur_length - 1]; p++) *p = *(p + 1);
+	
+	L->cur_length--;
 	
 	return 0;
 }
 
-ElemType SqListGetElem(SqList& L, int index) {
-	if(index < 0 || index > L.length) {
+int SqListGetElem(SqList* L, int index, ElemType* outElem) {
+	if(!L) {
+		//illegal operation
 		return -1;
 	}
 	
-	return L.elem_base_addr[index - 1];
+	if(index < 1 || index > L->cur_length) {
+		//parameter is not legal
+		return -1;
+	}
+	
+	if(!L->elem_base_addr) {
+		//SqList is not init
+		return -1;
+	}
+	
+	*outElem = L->elem_base_addr[index - 1];
+	return 0;
 }
 
-int SqListGetIndex(SqList& L, ElemType elem, int (*compare)(ElemType, ElemType)) {
+int SqListGetIndex(SqList* L, ElemType elem, int (*compare)(ElemType, ElemType)) {
+	if(!L) {
+		//illegal operation
+		return -1;
+	}
+	
+	if(!L->elem_base_addr) {
+		//SqList in not init
+		return -1;
+	}
+	
 	int index = 1;
 	
-	while(index <= L.length && !(*compare)(L.elem_base_addr[index - 1], elem)) index++;
+	while(index <= L->cur_length && !(*compare)(L->elem_base_addr[index - 1], elem)) index++;
 	
-	if(index <= L.length) {
+	if(index <= L->cur_length) {
 		return index;
 	}else{
 		return 0;
 	}
+}
+
+void SqListTraverse(SqList* L, void (*visit)(ElemType)) {
+	if(!L) {
+		return;
+	}
 	
-}
-
-int SqListLength(SqList& L) {
-	return L.cur_length;
-}
-
-int SqListEmpty(SqList& L) {
-	return L.cur_length == 0;
-}
-
-void SqListClear(SqList& L) {
-	L.cur_length = 0;
-}
-
-void SqListDestory(SqList& L) {
-	for(ElemType* p = &L.elem_base_addr[L.cur_length - 1]; p >= L.elem_base_addr; p--) free(p);
+	if(!L->elem_base_addr) {
+		return;
+	}
 	
-	L.elem_base_addr = NULL;
-	L.cur_length = 0;
-	L.max_length = 0;
+	int index = 0;
+	
+	while(index < L->cur_length) {
+		(*visit)(L->elem_base_addr[index++]);
+	}
+}
+
+int SqListLength(SqList* L) {
+	if(!L) {
+		//illegal operation
+		return -1;
+	}
+	
+	if(!L->elem_base_addr) {
+		//SqList in not init
+		return -1;
+	}
+	
+	return L->cur_length;
+}
+
+int SqListEmpty(SqList* L) {
+	if(!L) {
+		//illegal operation
+		return -1;
+	}
+	
+	if(!L->elem_base_addr) {
+		//SqList in not init
+		return -1;
+	}
+	
+	return L->cur_length == 0;
+}
+
+void SqListClear(SqList* L) {
+	if(!L) {
+		//illegal operation
+		return;
+	}
+	
+	if(!L->elem_base_addr) {
+		//SqList in not init
+		return;
+	}
+	
+	L->cur_length = 0;
+}
+
+void SqListDestory(SqList* L) {
+	if(!L) {
+		//illegal operation
+		return;
+	}
+	
+	if(!L->elem_base_addr) {
+		//SqList in not init
+		return;
+	}
+	
+	for(ElemType* p = &L->elem_base_addr[L->cur_length - 1]; p >= L->elem_base_addr; p--) free(p);
+	
+	L->elem_base_addr = NULL;
+	L->cur_length = 0;
+	L->max_length = 0;
 }
 
 //Assume that the SqList La and Lb are sorted, and the output Lc is sorted too
-void MergeSqList(SqList& La, SqList& Lb, SqList& Lc) {
-	InitSqList(Lc);
+void MergeSqList(SqList* La, SqList* Lb, SqList* Lc) {
+	if(!La || !Lb || !Lc) {
+		//illegal operation
+		return;
+	}
 	
-	int laIndex = 1;
-	int lbIndex = 1;
-	int lcIndex = 1;
+	if(!La->elem_base_addr || !Lb->elem_base_addr || !Lc->elem_base_addr) {
+		//SqList in not init
+		return;
+	}
 	
-	while(laIndex <= SqListLength(La) && lbIndex <= SqListLength(Lb)) {
-		ElemType laValue = SqListGetElem(laIndex);
-		ElemType lbValue = SqListGetElem(lbIndex);
+	int laIndex = 1, lbIndex = 1, lcIndex = 1;
+	ElemType laValue;
+	ElemType lbValue;
+		
+	while(laIndex <= SqListLength(La) && lbIndex <= SqListLength(Lb)) {		
+		SqListGetElem(La, laIndex, &laValue);
+		SqListGetElem(Lb, lbIndex, &lbValue);
+		
 		
 		if(laValue <= lbValue) {
 			SqListInsert(Lc, lcIndex++, laValue);
@@ -123,12 +239,78 @@ void MergeSqList(SqList& La, SqList& Lb, SqList& Lc) {
 	}
 	
 	while(laIndex <= SqListLength(La)) {
+		SqListGetElem(La, laIndex++, &laValue);
 		SqListInsert(Lc, lcIndex++, laValue);
-		laIndex++;
 	}
 	
 	while(lbIndex <= SqListLength(Lb)) {
+		SqListGetElem(Lb, lbIndex++, &lbValue);
 		SqListInsert(Lc, lcIndex++, lbValue);
-		lbIndex++;
 	}
+}
+
+//just for test, if the ElemType is not int, error
+void visit(ElemType elem) {
+	printf("%d ", elem);
+}
+
+int main(int argc, char** argv) {
+	SqList La, Lb;
+	
+	//init sqlist a
+	InitSqList(&La);
+	for(int i = 1, data = 1; i <= 10; i++, data += 2) {
+		SqListInsert(&La, i, data);
+	}
+	
+	printf("Init La data:");
+	SqListTraverse(&La, visit);
+	printf("\n");
+	
+	SqListClear(&La);
+	
+	printf("La data after clear:");
+	SqListTraverse(&La, visit);
+	printf("\n");
+	
+	
+	for(int i = 1, data = 1; i <= 5; i++, data++) {
+		SqListInsert(&La, i, data);
+	}
+	printf("La data reconstruct:");
+	SqListTraverse(&La, visit);
+	printf("\n");
+	
+	//init sqlist b
+	InitSqList(&Lb);
+	for(int i = 1, data = 0; i <= 10; i++, data += 2) {
+		SqListInsert(&Lb, i, data);
+	}
+	
+	printf("Init Lb data:");
+	SqListTraverse(&Lb, visit);
+	printf("\n");
+	
+	SqListDelete(&Lb, 1);
+	SqListDelete(&Lb, 1);
+	SqListDelete(&Lb, SqListLength(&Lb));
+	printf("Lb data after delete:");
+	SqListTraverse(&Lb, visit);
+	printf("\n");
+	
+	ElemType data;
+	SqListGetElem(&Lb, 3, &data);
+	printf("Lb data 3th:%d", data);
+	printf("\n");
+	
+	SqList Lc;
+	//init sqlist a
+	InitSqList(&Lc);
+	MergeSqList(&La, &Lb, &Lc);
+	
+	printf("Merge Lc data:");
+	SqListTraverse(&Lc, visit);
+	printf("\n");
+	
+	return 0;
 }
